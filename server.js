4b667b119d9db22a42aa2778259f614a1cc85348
rumanajaha <rumanajaha1@gmail.com>
2025-10-16 20:55:06 +0530

    Initial ridepooling_backend setup

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send(' Ridepooling Backend is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
